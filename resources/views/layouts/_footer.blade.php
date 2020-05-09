55ffeb026a413337605d821d90db11b79b3e1c39
Your Name <you@example.com>
2020-05-09 16:37:29 +0800

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<div class="container">
+  <p class="float-left">
+    由 <a href="" target="_blank">Leon</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+  </p>
+
+  <p class="float-right"><a href="mailto:752075717@qq.com">联系我们</a></p>
+</div>
+</footer>
