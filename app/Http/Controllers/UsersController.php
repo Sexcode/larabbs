6f93bd72289476b68ee517537619e20c1e0c1ffe
Your Name <you@example.com>
2020-05-12 18:02:07 +0800

    显示个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,9 +20,8 @@ class UsersController extends Controller
 
     public function update(UserRequest $request, User $user)
     {
-//        $user->update($request->all());
-        echo 1;exit;
-//        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
     }
 
 }
