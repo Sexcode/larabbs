4e6613af7e8a5a2beb4b8c253c662e16083a0723
Your Name <you@example.com>
2020-05-14 17:34:46 +0800

    生成话题股价

--- database/migrations/2020_05_14_165418_seed_categories_data.php
+++ database/migrations/2020_05_14_165418_seed_categories_data.php
@@ -15,8 +15,22 @@ class SeedCategoriesData extends Migration
     {
         //
         $categories = [
-//            ['name'=>'','description'=>''],
-//            ['name'=>'','description'=>''],
+            [
+                'name'        => '分享',
+                'description' => '分享创造，分享发现',
+            ],
+            [
+                'name'        => '教程',
+                'description' => '开发技巧、推荐扩展包等',
+            ],
+            [
+                'name'        => '问答',
+                'description' => '请保持友善，互帮互助',
+            ],
+            [
+                'name'        => '公告',
+                'description' => '站点公告',
+            ],
         ];
         DB::table('categories')->insert($categories);
     }
