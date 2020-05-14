2043e471f7b888f4699bf8a54d732584c771a854
Your Name <you@example.com>
2020-05-14 16:57:58 +0800

    分类模型和数据

---
+++ database/migrations/2020_05_14_165418_seed_categories_data.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SeedCategoriesData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        $categories = [
+//            ['name'=>'','description'=>''],
+//            ['name'=>'','description'=>''],
+        ];
+        DB::table('categories')->insert($categories);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        DB::table('categories')->truncate();
+    }
+}
